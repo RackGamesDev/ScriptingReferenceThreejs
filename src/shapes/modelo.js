ca7eb2f3f44bddffe59d32d3f26222a33ba6744f
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 01:01:03 +0100

    Update modelo.js

--- src/shapes/modelo.js
+++ src/shapes/modelo.js
@@ -1,16 +1,16 @@
-//importar la geometria de un fbx
+//Importar la geometria de un fbx
 import * as THREE from '../js/three.js';
-import {FBXLoader} from '../js/addons/FBXLoader.js';//addon oficial de threejs
+import {FBXLoader} from '../js/addons/FBXLoader.js';//Addon oficial de threejs
 const loader = new FBXLoader();
 const model = new Promise((res, rej) => {
-    loader.load('src/shapes/modelo.fbx', (obj) => {//ruta del fbx
+    loader.load('src/shapes/modelo.fbx', (obj) => {//Ruta del fbx
         obj.traverse(function (child) {
             if(child.isMesh){
                 child.castShadow = true;
                 child.recieveShadow = true;
             }
         });
-        //const material = new THREE.MeshStandardMaterial({color: 0x00ff00});//agregarle el material
+        //const material = new THREE.MeshStandardMaterial({color: 0x00ff00});//Agregarle el material
         //const objeto = new THREE.Mesh(obj, material);
         res(obj);
     });
